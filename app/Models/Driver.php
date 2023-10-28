ebee55590c4920f188f78279a1d4f4933f5f1574
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-28 22:08:40 +0800

    Update Driver.php

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class Driver extends Model
+class Driver extends Model implements AuthenticatableContract
 {
-    use HasFactory;
+    use HasFactory, Authenticatable;
+
+    protected $table = 'drivers';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'driver_email',
+        'driver_password',
+        // Add any other driver-specific fields here
+    ];
+
+    protected $hidden = [
+        'driver_password',
+    ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['driver_password'] = bcrypt($value);
+    }
+
+    public function username()
+    {
+        return 'driver_email';
+    }
 }
