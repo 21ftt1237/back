235e9901df95f10130fb306e0f60f182eb59b75c
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-28 23:08:51 +0800

    Update driver-login.blade.php

--- resources/views/driver-login.blade.php
+++ resources/views/driver-login.blade.php
@@ -8,10 +8,10 @@
     <form method="POST" action="{{ route('driver.login') }}">
         @csrf <!-- Include a CSRF token for security -->
         <label for="email">Email Address:</label>
-        <input type="email" id="email" name="email" required><br><br>
+        <input type="email" id="driver_email" name="driver_email" required><br><br>
 
         <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
+        <input type="password" id="driver_password" name="driver_password" required><br><br>
 
         <input type="submit" value="Login">
     </form>
