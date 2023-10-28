6de86e11badf65d9e5d14221b581fc11919e32cc
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-28 23:06:06 +0800

    Update driverdash.blade.php

--- resources/views/driverdash.blade.php
+++ resources/views/driverdash.blade.php
@@ -3,6 +3,9 @@
             {{ __('Dashboard') }}
   </h2>
 
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    @csrf
+</form>
 <a href="{{ route('driver.logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
     Logout
 </a>
