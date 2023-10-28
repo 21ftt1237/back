2946bb63bae6cf3b4fa987ef54f141fc32a44593
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-29 00:57:41 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,79 @@ Route::get('/register', [DriverController::class, 'showRegistrationForm'])->name
 
     // Driver Logout Route
     Route::post('/logout', [DriverController::class, 'logout'])->name('driver.logout');
+
+Route::get('netcom', function () {
+    return view('netcom');
+})->name('netcom');
+
+Route::get('order', function () {
+    return view('My order.order');
+})->name('order');
+
+Route::get('Deli', function () {
+    return view('Deli');
+});
+
+Route::get('Guardian', function () {
+    return view('Guardian');
+})->name('Guardian');
+
+Route::get('searchFilter', function () {
+    return view('searchFilter');
+})->name('searchFilter');
+
+Route::get('avenue', function () {
+    return view('avenue');
+})->name('avenue');
+
+Route::get('Nimanja', function () {
+    return view('Nimanja');
+})->name('Nimanja');
+
+Route::get('digital', function () {
+    return view('digital');
+})->name('digital');
+
+Route::get('BruZoneWishlist', function () {
+    return view('Wishlist.BruZoneWishlist'); 
+})->name('BruZoneWishlist');
+
+Route::get('BruzoneFAQ', function () {
+    return view('Help.BruzoneFAQ'); 
+})->name('BruzoneFAQ');
+
+Route::get('BruzoneEmail', function () {
+    return view('Help.BruzoneEmail'); 
+})->name('BruzoneEmail');
+
+Route::get('storePlatform-SO', function () {
+    return view('storePlatform-SO'); 
+})->name('storePlatform-SO');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('gamecentral', function () {
+    return view('gamecentral');
+})->name('gamecentral');
+
+Route::get('checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+
+Route::get('profilev', [ProfileController::class, 'showProfile'])->name('profilev');
+
+Route::post('/redeem-coupon', [ProfileController::class, 'redeemCoupon'])->name('redeemCoupon');
+
+Route::post('profilev', [ProfileController::class, 'profupdate'])->name('updateProfile');
+
+Route::get('coupon', [ProfileController::class, 'showLoyal'])->name('coupon');
+
