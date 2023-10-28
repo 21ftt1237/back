6657b53db0e5edca89e252c89566afebb7dad3ec
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-28 23:03:42 +0800

    Update driverdash.blade.php

--- resources/views/driverdash.blade.php
+++ resources/views/driverdash.blade.php
@@ -2,3 +2,7 @@
   <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
   </h2>
+
+<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+    Logout
+</a>
