84f6f4785640787ac67b87f9bbc3f2f7ff8d801a
21ftt1237 <21ftt1237@student.pb.edu.bn>
2023-10-29 06:17:00 +0000

    pe

---
+++ database/migrations/2023_10_28_174727_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('store_id');
+            $table->string('name')->nullable();
+            $table->foreign('store_id')->references('id')->on('stores');
+            $table->decimal('price', 10, 2); // Assuming a decimal type for price
+            $table->text('description')->nullable();
+            $table->string('image_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
