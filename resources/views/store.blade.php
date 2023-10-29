4046501a4edf45874b611133be697813b81f0dbc
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-29 14:02:24 +0800

    Update store.blade.php

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -2,4 +2,29 @@
     $pageName = 'store';
 @endphp
 @include('layouts.header')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sample HTML Page</title>
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+    <p>This is a sample HTML page.</p>
+
+    <h2>About Me</h2>
+    <p>Hello, I'm John Doe. I enjoy creating web pages.</p>
+
+    <h2>My Interests</h2>
+    <ul>
+        <li>Web Development</li>
+        <li>Programming</li>
+        <li>Design</li>
+    </ul>
+
+    <h2>Contact Information</h2>
+    <p>Email: john.doe@example.com</p>
+
+</body>
+</html>
+
 @include('layouts.footer')
