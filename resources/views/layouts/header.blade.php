9ae63f535c6e3c6c5c024d462e83c52e4656ba05
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-29 19:32:08 +0800

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,7 +15,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
     <title>Your Page Title</title>
     <style>
  /* HELP dropdown styles */
