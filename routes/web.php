857a960f509d8e1fecb0f43cd576576230fa0347
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-31 03:24:49 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -139,6 +139,6 @@ Route::get('store', [ProductController::class, 'index']);
 
 Route::post('/save-cart-items', [CartController::class, 'saveCartItems']);
 
-Route::get('/order', [CartController::class, 'getCartItems']);
+Route::get('/oi', [CartController::class, 'getCartItems']);
 
 require __DIR__.'/auth.php';
