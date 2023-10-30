ad65c8ab3aba245e2950df44485dceec949cab5c
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-31 02:58:23 +0800

    Update order.blade.php

--- resources/views/My order/order.blade.php
+++ resources/views/My order/order.blade.php
@@ -196,6 +196,7 @@
      <div class="container">
 
     </div>
+     <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -219,7 +220,7 @@
             </div>
         </div>
     </div>
-
+ </x-app-layout>
     
 </body>
 </html>
