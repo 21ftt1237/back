edcebde62d9bac8f3a5988c71fb06010621a5039
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-31 03:20:02 +0800

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -67,6 +67,6 @@ class CartController extends Controller
         return $item->price * $item->quantity;
     });
 
-    return view('order.order', compact('cartItems', 'totalQuantity', 'totalPrice'));
+    return view('My order.order', compact('cartItems', 'totalQuantity', 'totalPrice'));
 }
 }
