124a5c505907887050560df769b1023f09c42b05
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-31 03:03:53 +0800

    Update order.blade.php

--- resources/views/My order/order.blade.php
+++ resources/views/My order/order.blade.php
@@ -191,17 +191,11 @@
 
    
 </head>
- <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-    
+ <body> 
      <div class="container">
-
     </div>
     
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+
                     
     <div class="data">
         <div class="eclipse-container">
@@ -217,11 +211,9 @@
                      
                        </div>
                      </div>
-                   </div>
-                 </div>
-              </div>
-            </div>          
-          </div>
+                   
+              
+         
  
     
 </body>
