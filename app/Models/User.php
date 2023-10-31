f57b5a936f54b1ccc14bdbf57e69a5d6b65388ba
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-01 00:45:23 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable
 
     public function wishlist(): BelongsToMany
 {
-    return $this->belongsToMany(Product::class, 'user_wishlist')->withTimestamps();
+    return $this->belongsToMany(Product::class, 'wishlist')->withTimestamps();
 }
     
 }
