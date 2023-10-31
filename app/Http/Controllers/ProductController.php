0a1d1d8ff989eeb2c2bb448623d2b4096b41f19f
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-01 03:10:26 +0800

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -64,18 +64,6 @@ public function removeFromWishlist(Request $request, Product $product) {
     return redirect()->back()->with('success', 'Product removed from wishlist.');
 }
 
-public function indexBruzoneWishlist()
-{   \Log::info('indexBruzoneWishlist method called');
-    dd('Method executed');
-    // Get the currently authenticated user
-    $user = auth()->user();
-
-    // Retrieve the wishlist products for the user
-    $wishlistProducts = $user->wishlist;
-
-    return view('Wishlist.BruZoneWishlist', compact('wishlistProducts'));
-}
-
 
     
 }
