7704b0e8b2a2dd30e72ffcde57c6df22b000020f
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-01 00:43:19 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable
 {
@@ -47,4 +48,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function wishlist(): BelongsToMany
+{
+    return $this->belongsToMany(Product::class, 'user_wishlist')->withTimestamps();
+}
+    
 }
