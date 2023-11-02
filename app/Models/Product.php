a7f4a376cd7c1be7e6173d9bfb4d7a6382427a53
21ftt1225 <98934997+21ftt1225@users.noreply.github.com>
2023-11-02 22:10:59 +0800

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -18,6 +18,7 @@ class Product extends Model
     public function users(): BelongsToMany
 {
     return $this->belongsToMany(User::class, 'wishlist')->withTimestamps();
+    return $this->belongsToMany(User::class, 'cart')->withTimestamps();
 }
 
 }
