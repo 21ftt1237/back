04d7131327f9130b5012213a87f100062dcbc921
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-03 03:12:16 +0800

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use App\Models\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
