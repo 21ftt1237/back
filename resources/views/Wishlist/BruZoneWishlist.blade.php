2b184708d96675a250e8d631caed3e8ccdb537fc
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-02 22:47:20 +0800

    Update BruZoneWishlist.blade.php

--- resources/views/Wishlist/BruZoneWishlist.blade.php
+++ resources/views/Wishlist/BruZoneWishlist.blade.php
@@ -507,6 +507,7 @@ header .shopping img{
             <div class="item-name">{{ $wishlistItem->product->name }}</div>
             <div class="item-price">BND {{ $wishlistItem->product->price }}</div>
         </div>
+        <button class="add-to-cart-button">Add to Cart</button>
         <form action="{{ route('wishlist.remove', ['product' => $wishlistItem->product]) }}" method="POST">
             @csrf
             <button type="submit" class="remove-button">Remove</button>
