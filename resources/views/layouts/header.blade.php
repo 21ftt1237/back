93ddcd0b6b77d632aaf1ff1ac82d14e2adcb37b3
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-03 00:28:25 +0800

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -117,7 +117,7 @@
         <div class="checkOut" id="">
             <a href="{{ route('checkout') }}">
             <div class="total">BND 0</div></a>
-        <div>
+        <div id="shopping-cart" class="shopping-cart active">
         <button id="closeShopping" class="close-button" onclick="closeShoppingCart()">
         <i class="fas fa-times-circle"></i></div>    
         </button>
