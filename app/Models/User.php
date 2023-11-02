81134ca2873261de94f8832c032fafd8d8715f6d
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-02 22:08:45 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,6 +33,11 @@ class User extends Authenticatable
         return $this->hasMany(CartItem::class);
     }
 
+     public function wishlist()
+    {
+        return $this->hasMany(Cart::class);
+    }
+
 
 }
 
