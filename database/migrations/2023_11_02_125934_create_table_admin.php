86d36bd5a50bf093de0655bb1bcb8512b86cf351
21ftt1237 <21ftt1237@student.pb.edu.bn>
2023-11-03 13:54:10 +0000

    hafiy

--- database/migrations/2023_11_02_125934_create_table_admin.php
+++ database/migrations/2023_11_02_125934_create_table_admin.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('password');
 	    $table->unsignedBigInteger('role_id');
             $table->timestamps();
-
 	    $table->foreign('role_id')->references('id')->on('table_roles');
 
         });
