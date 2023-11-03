e3bbd5bcdda13e89916cfc70bae31bfdd734b6b8
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-03 22:51:28 +0800

    Update netcom.blade.php

--- resources/views/netcom.blade.php
+++ resources/views/netcom.blade.php
@@ -10,7 +10,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="stylesheet" href="./ecommerce.css">
   <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
 
