5a20722682cc95b0302f1548fc296b11f1b4267c
21ftt1237 <21ftt1237@student.pb.edu.bn>
2023-11-03 15:03:00 +0000

    hafiy

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,16 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function index()
+    
+    {
+        // Retrieve admins with their roles
+        $admins = Admin::with('role')->get();
+
+        // You can return a view and pass the $admins data to display it
+        return view('admins.index', compact('admins'));
+    }
+
     public function ChangeStatuss (Request $request){
 
 
