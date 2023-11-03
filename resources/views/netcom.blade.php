f83b7331a0b25b53c04141e03ccca68e308d82be
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-04 01:01:02 +0800

    Update netcom.blade.php

--- resources/views/netcom.blade.php
+++ resources/views/netcom.blade.php
@@ -13,7 +13,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="stylesheet" href="./ecommerce.css">
   <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
-
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <!-- Other meta tags and styles -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
