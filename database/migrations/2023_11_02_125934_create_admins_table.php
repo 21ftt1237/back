524a1703779d2e1ef621038e10a0e9bbaf27f9ff
21ftt1237 <21ftt1237@student.pb.edu.bn>
2023-11-04 20:37:31 +0000

    admin table

--- database/migrations/2023_11_02_125934_create_admins_table.php
+++ database/migrations/2023_11_02_125934_create_admins_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_admin', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
 	    $table->string('name');
             $table->string('email')->unique();
@@ -29,9 +29,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('table_admin', function (Blueprint $table) {
+        Schema::table('admins', function (Blueprint $table) {
             $table->dropForeign(['role_id']);
         });
-        Schema::dropIfExists('table_admin');
+        Schema::dropIfExists('admins');
     }
 };
