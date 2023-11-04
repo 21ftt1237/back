4a02850a7051f5bf2ce02b513470f8f8e000adc0
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-04 14:00:05 +0800

    Update Cart.php

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -7,24 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    // Define the table associated with this model (optional, Laravel will assume "wishlists" based on the class name)
+   
     protected $table = 'cart';
 
-    // Define the fillable fields (columns that can be mass-assigned)
+   
     protected $fillable = [
         'product_id',
         'user_id',
         'quantity',
-        // Other wishlist fields, if any
+        
     ];
 
-    // Define a relationship with the Product model
+   
     public function product()
     {
         return $this->belongsTo(Product::class, 'product_id');
     }
 
-    // Define a relationship with the User model
+  
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
