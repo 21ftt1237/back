747373a5a2b9f8be1760bba03bf57a03f8bf04b6
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-04 19:48:40 +0800

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class OrderController extends Controller
 {
@@ -25,6 +26,7 @@ public function updateCouponPoint(Request $request)
 
     // Convert the incoming coupon point value to a decimal
     $couponPoint = floatval($request->coupon_point);
+    Log::info('Coupon Points: ' . $couponPoints);
 
     // Update the user's "coupon_point" points
     $user->coupon_point += $couponPoint;
