52be37924faa149084b33e4b311817dc0bfad274
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-04 18:29:30 +0800

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1,1 +1,32 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+   
+    protected $table = 'orders';
+
+   
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'quantity',
+        
+    ];
+
+   
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+  
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
