f9356704f13c2f6457d3e105a4eedc78a3e59ded
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-04 19:51:34 +0800

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,25 +14,37 @@ use Illuminate\Support\Facades\Log;
 class OrderController extends Controller
 {
 
-public function updateCouponPoint(Request $request)
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class OrderController extends Controller
 {
-    // Validate the request
-    $request->validate([
-        'coupon_point' => 'required|numeric', // numeric allows decimal values
-    });
+    public function updateCouponPoint(Request $request)
+    {
+        // Validate the request
+        $request->validate([
+            'coupon_point' => 'required|numeric', // numeric allows decimal values
+        ]);
 
-    // Get the currently authenticated user
-    $user = auth()->user();
+        // Get the currently authenticated user
+        $user = Auth::user();
 
-    // Convert the incoming coupon point value to a decimal
-    $couponPoint = floatval($request->coupon_point);
-    Log::info('Coupon Points: ' . $couponPoints);
+        // Convert the incoming coupon point value to a decimal
+        $couponPoint = floatval($request->coupon_point);
+        Log::info('Coupon Points: ' . $couponPoint);
 
-    // Update the user's "coupon_point" points
-    $user->coupon_point += $couponPoint;
-    $user->save();
+        // Update the user's "coupon_point" points
+        $user->coupon_point += $couponPoint;
+        $user->save();
 
-    return response()->json(['message' => 'Coupon points updated successfully']);
-}
+        return response()->json(['message' => 'Coupon points updated successfully']);
+    }
+
 
 }
