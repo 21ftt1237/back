9a813d739f98818edf30c296fc324c850d9fe785
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-05 04:17:15 +0800

    Update AdminAuthController.php
    models

--- app/Http/Controllers/Auth/AdminAuthController.php
+++ app/Http/Controllers/Auth/AdminAuthController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
+use App\Models\Admin;
+
 
 class AdminAuthController extends Controller
 {
