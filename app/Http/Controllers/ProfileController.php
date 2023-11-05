4c5095fc48516edf44a18cbb9fb866e4c3701455
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-06 00:43:48 +0800

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -59,58 +59,58 @@ class ProfileController extends Controller
         return Redirect::to('/');
     }
 
-    public function showProfile()
+    public function showCoupon()
 {
-    // Ensure the user is authenticated
+   
     if (Auth::check()) {
-        // Fetch the logged-in user
+      
         $user = Auth::user();
 
-        // Get the loyalty points from the user model
+       
         $loyaltyPoints = $user->coupon_point;
 
-        // Get the redeem_coupon value
+       
         $redeemCoupon = $user->redeem_coupon;
 
-         // Pass the loyalty points and redeem_coupon to the view
+        
         return view('profilev', ['loyaltyPoints' => $loyaltyPoints, 'redeemCoupon' => $redeemCoupon]);
     }
 
-    // Handle the case when the user is not authenticated
+    
     return redirect('/login');
 }
 
 public function redeemCoupon(Request $request)
 {
-    // Check if the user is authenticated
+    
     $user = Auth::user();
 
-    // Check if the user is null
+   
     if (!$user) {
         return redirect()->back()->with('error', 'User not authenticated.');
     }
 
-    // Validate the 'coupon_amount' input
+   
     $couponAmount = $request->input('coupon_amount');
 
     if (!is_numeric($couponAmount) || $couponAmount <= 0) {
         return redirect()->back()->with('error', 'Invalid coupon amount.');
     }
 
-    // Check if the user has already redeemed a coupon
+    
     if ($user->redeem_coupon !== null && $user->redeem_coupon > 0) {
         return redirect()->back()->with('message', 'You have already redeemed a coupon.');
     }
 
-    // Check if the user has enough loyalty points to redeem the coupon
+    
     if ($user->coupon_point >= $couponAmount) {
         // Deduct loyalty points
         $user->coupon_point -= $couponAmount;
 
-        // Divide coupon amount by 100
+        
         $couponAmount /= 100;
 
-        // Store coupon amount
+        
         $user->redeem_coupon = $couponAmount;
         $user->save();
 
@@ -121,20 +121,14 @@ public function redeemCoupon(Request $request)
 }
 
 
-    // method to update profile
+    
 public function profupdate(Request $request)
 {
     $userId = $request->input('id');
 
-    // Use a database query to find the user by their ID
+   
     $user = DB::table('users')->where('id', $userId)->first();
 
-    if (!$user) {
-        // Handle the case where the user is not found.
-        // You can redirect back with an error message.
-    }
-
-    // Update user's information
     $updateData = [
         'name' => $request->input('name'),
         'mobile_number' => $request->input('phone_number'),
@@ -144,7 +138,6 @@ public function profupdate(Request $request)
         'district' => $request->input('district'),
     ];
 
-    // Use a database query to update the user's record
     DB::table('users')->where('id', $userId)->update($updateData);
 
     return redirect()->route('profilev')->with('message', 'Profile updated successfully');
@@ -152,49 +145,49 @@ public function profupdate(Request $request)
 
     public function showLoyal()
 {
-    // Ensure the user is authenticated
+   
     if (Auth::check()) {
-        // Fetch the logged-in user
+      
         $user = Auth::user();
 
-        // Get the loyalty points from the user model
+      
         $loyaltyPoints = $user->coupon_point;
 
-        // Get the redeem_coupon value
+      
         $redeemCoupon = $user->redeem_coupon;
 
-         // Pass the loyalty points and redeem_coupon to the view
+         
         return view('coupon', ['loyaltyPoints' => $loyaltyPoints, 'redeemCoupon' => $redeemCoupon]);
     }
 
-    // Handle the case when the user is not authenticated
+   
     return redirect('/login');
 }
 public function calculateLoyaltyPoints(Request $request) {
-    // Validate the request data
+  
     $request->validate([
         'totalPrice' => 'required|numeric',
     ]);
 
-    // Get the total price from the request
+    
     $totalPrice = $request->input('totalPrice');
 
-    // Calculate loyalty points using a hypothetical function
+    
     $loyaltyPoints = $this->calculateLoyaltyPointsForPrice($totalPrice);
 
-    // Find the user (you may need to adjust this part to retrieve the user based on your authentication system)
+   
     $user = Auth::user();
 
     if (!$user) {
         return response()->json(['message' => 'User not found'], 404);
     }
 
-    // Use a database transaction to ensure data consistency
+   
     DB::transaction(function () use ($user, $loyaltyPoints) {
-        // Add the calculated loyalty points to the user's coupon points
+       
         $user->coupon_points += $loyaltyPoints;
 
-        // Save the user
+        
         $user->save();
 
         return response()->json(['message' => 'Loyalty points added successfully']);
@@ -208,7 +201,7 @@ public function updateLoyaltyPoints(Request $request)
     $userId = auth()->user()->id; // Get the currently logged-in user's ID
     $loyaltyPoints = $request->input('loyaltyPoints'); // Get the loyalty points value from the request
 
-    // Update the user's loyalty points in the users table
+   
     User::where('id', $userId)->update(['coupon_point' => $loyaltyPoints]);
 
     return response()->json(['message' => 'Coupon points updated successfully']);
