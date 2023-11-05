37c3bb1f988dfda2b2d65685e3136dc7eac0c53c
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-05 17:30:25 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -193,7 +193,7 @@ Route::post('/update-coupon-point', [OrderController::class, 'updateCouponPoint'
 
 Route::post('/place-order', [OrderController::class, 'placeOrder']);
 
-Route::get('/order-list', [OrderController::class,'showOrderList']);
+Route::get('/order-list', [OrderController::class,'showOrderList'])->name('order.add');
 
 //ETC
 Route::get('/email', 'EmailController@index');
