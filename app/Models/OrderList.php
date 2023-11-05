0bcd74f76d50c2c4c76580c5ce8187e3b860905b
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-05 12:37:22 +0800

    Update OrderList.php

--- app/Models/OrderList.php
+++ app/Models/OrderList.php
@@ -4,21 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use App\Models\Order;
-
-
 
 class OrderList extends Model
 {
     use HasFactory;
 
- protected $table = 'orders_list';
+    protected $table = 'orders_list';
 
-
-public function orderLists()
-{
-    return $this->hasMany(OrderList::class);
-}
-    
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
