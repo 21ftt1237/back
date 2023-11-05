17668ef19207f35450079a9170825b7ccdbfa0eb
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-06 00:29:30 +0800

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -15,25 +15,25 @@ class CartController extends Controller
 public function index()
 {
     // Fetch the currently logged-in user's wishlist items
-    $user = auth()->user(); // Get the currently authenticated user
+    $user = auth()->user(); 
     $cart = Cart::where('user_id', $user->id)
-        ->with('product') // Assuming you have defined a 'product' relationship in the Wishlist model
+        ->with('product') 
         ->get();
 
     return view('layouts.header', compact('cart'));
 }
     public function indexCheckout()
 {
-    // Fetch the currently logged-in user's wishlist items
+    //display cart item into checkout
     $user = auth()->user(); // Get the currently authenticated user
     $cart = Cart::where('user_id', $user->id)
-        ->with('product') // Assuming you have defined a 'product' relationship in the Wishlist model
+        ->with('product') 
         ->get();
 
     return view('checkout', compact('cart'));
 }
 
-        public function increaseQuantity(Request $request, Product $product) {
+    public function increaseQuantity(Request $request, Product $product) {
     $user = $request->user();
     $cartItem = $user->cart()->where('product_id', $product->id)->first();
 
@@ -61,30 +61,6 @@ public function index()
     }
 
     return redirect()->back()->with('success', 'Quantity decreased.');
-}
-
-public function addToCart(Request $request)
-{
-    // Get the product_id from the request
-    $productId = $request->input('product_id');
-    
-    // You can use your authentication logic to get the currently authenticated user
-    $user = auth()->user();
-
-    // Check if the product is already in the user's cart
-    $cartItem = $user->cart()->where('product_id', $productId)->first();
-
-    if ($cartItem) {
-        // If the product is already in the cart, increase its quantity
-        $cartItem->pivot->quantity += 1;
-        $cartItem->pivot->save();
-    } else {
-        // If the product is not in the cart, add it with a quantity of 1
-        $user->cart()->attach($productId, ['quantity' => 1]);
-    }
-
-    // Return a response (e.g., JSON) indicating success
-    return response()->json(['message' => 'Product added to cart.']);
 }
 
   
