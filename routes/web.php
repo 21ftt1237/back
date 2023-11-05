a9508f8594779f3464669e6092bd714df458c0d9
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-06 05:54:19 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -210,22 +210,7 @@ Route::post('/email/send', 'EmailController@sendEmail')->name('send.email');
 
 Route::prefix('admin')->middleware(['auth'])->group(function () {
 
-    
-Route::get('/admins', [AdminController::class, 'index'])->name('admins.index');
-
-Route::get('/admins/create', [AdminController::class, 'create'])->name('admins.create');
-
-Route::post('/admins', [AdminController::class, 'store'])->name('admins.store');
-
-Route::get('/admins/{admin}', [AdminController::class, 'show'])->name('admins.show');
-
-Route::get('/admins/{admin}/edit', [AdminController::class, 'edit'])->name('admins.edit');
-
-Route::put('/admins/{admin}', [AdminController::class, 'update'])->name('admins.update');
-
-Route::delete('/admins/{admin}', [AdminController::class, 'destroy'])->name('admins.destroy');
-
-    Route::get('/admin/adminDashboard', 'AdminController@index')->name('admin.dashboard');
+    Route::get('/adminDashboard', 'AdminController@index')->name('admin.dashboard');
 
     //login and logout
     Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
