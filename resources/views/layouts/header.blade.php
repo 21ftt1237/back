5837d748c90badaa1065e60b452432a6cf462f9a
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-05 19:17:23 +0800

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -83,7 +83,7 @@ $totalQuantity += $cartItem->quantity; // Add the quantity of the item to the to
                <div class="logo" id="storeName" data-value=" @if(isset($storehere)) {{ $storenumber}} @endif"><a href="{{ route('dashboard') }}"> <img src="image/BruZone_Logo.png" style="width: 50px">Bruzone</a></div>
                  @if(isset($carts))
                  <div class="shopping">
-                <img src="{{ asset('image/shoppingcart.png') }}">
+                <img src="{{ asset('image/shoppingCart.png') }}">
                 <span class="quantity">{{ $totalQuantity }}</span>
                 </div>
                  @endif
