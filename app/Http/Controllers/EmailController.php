4e997bdd618354e0d32045fa703f9f8fc3e9a6d0
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-09 10:38:59 +0800

    Update EmailController.php

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -13,23 +13,30 @@ class EmailController extends Controller
         return view('Help.BruzoneEmail');
     }
 
-    public function sendEmail(Request $request)
-    {
-        // Validate the form input
-        $request->validate([
-            'from' => 'required|email',
-            'subject' => 'required|string',
-            'message' => 'required|string',
-        ]);
+  public function sendEmail(Request $request)
+{
+    // Validate the form input
+    $request->validate([
+        'from' => 'required|email',
+        'subject' => 'required|string',
+        'message' => 'required|string',
+    ]);
 
-        // Send the email
-        Mail::to('rekkolosta@gufum.com')->send(new EmailToGmail(
-            $request->input('subject'),
-            $request->input('from'),
-            $request->input('message')
-        ));
+    // Log a message indicating the validation passed
+    Log::info('Form validation passed.');
 
-        // Redirect to a thank you page or back to the form
-        return redirect('/thank-you');
-    }
+    // Send the email
+    Mail::to('rekkolosta@gufum.com')->send(new EmailToGmail(
+        $request->input('subject'),
+        $request->input('from'),
+        $request->input('message')
+    ));
+
+    // Log a message indicating that the email was sent
+    Log::info('Email sent successfully.');
+
+    // Redirect to a thank you page or back to the form
+    return redirect('/thank-you');
+}
+
 }
