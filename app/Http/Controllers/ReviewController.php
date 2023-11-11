a053437e764068e12911b19f6b941cdc48b8e5af
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-11 22:12:45 +0800

    Update ReviewController.php

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -9,19 +9,23 @@ use Illuminate\Support\Facades\Auth;
 class ReviewController extends Controller
 {
     
-public function store(Request $request)
+public function submitReview(Request $request)
     {
-        $request->validate([
-            'stars' => 'required|integer|min:1|max:5',
-            'review' => 'required|string',
+        $validatedData = $request->validate([
+            'rating' => 'required|integer|min:1|max:5',
+            'review' => 'required|string|max:255',
         ]);
 
-        // Store the review associated with the currently logged-in user
-        $user = Auth::user();
-        $review = new Review;
-        $review->stars = $request->input('stars');
-        $review->review = $request->input('review');
-        $user->reviews()->save($review);
+        // Assuming you have an authenticated user
+        $user_id = auth()->id();
+
+        // Create a new review
+        Review::create([
+            'user_id' => $user_id,
+            'store_id' => $request->input('store_id'), // You may need to pass the store_id from the front end
+            'rating' => $validatedData['rating'],
+            'review' => $validatedData['review'],
+        ]);
 
         return response()->json(['message' => 'Review submitted successfully']);
     }
