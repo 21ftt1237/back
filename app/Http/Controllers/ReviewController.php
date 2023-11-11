b8edc23f8583d08072a3368b464de6d7da46c719
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-11 23:16:27 +0800

    Update ReviewController.php

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -11,25 +11,42 @@ class ReviewController extends Controller
     
 public function store(Request $request)
     {
-        // Validate the request data
-        $request->validate([
-            'review' => 'required|string',
-            'rating' => 'required|integer',
-        ]);
+        try {
+            // Validate the request data
+            $request->validate([
+                'review' => 'required|string',
+                'rating' => 'required|integer',
+            ]);
 
-        // Get the currently logged user's ID
-        $userId = auth()->id();
+            // Get the currently logged user's ID
+            $userId = auth()->id();
 
-        // Create a new review record
-        $review = Review::create([
-            'user_id' => $userId,
-            'review' => $request->input('review'),
-            'rating' => $request->input('rating'),
-            // Add store_id if you have a way to associate the review with a store
-        ]);
+            // Create a new review record
+            $review = Review::create([
+                'user_id' => $userId,
+                'review' => $request->input('review'),
+                'rating' => $request->input('rating'),
+                // Add store_id if you have a way to associate the review with a store
+            ]);
 
-        // Redirect or return a response as needed
-        return redirect()->back()->with('success', 'Review submitted successfully!');
+            // Log success
+            Log::info('Review submitted successfully.', [
+                'user_id' => $userId,
+                'review_id' => $review->id,
+            ]);
+
+            // Redirect or return a response as needed
+            return redirect()->back()->with('success', 'Review submitted successfully!');
+        } catch (\Exception $e) {
+            // Log error
+            Log::error('Error submitting review.', [
+                'user_id' => $userId ?? null,
+                'error' => $e->getMessage(),
+            ]);
+
+            // Redirect or return a response for error handling
+            return redirect()->back()->with('error', 'An error occurred while submitting the review.');
+        }
     }
     
 }
