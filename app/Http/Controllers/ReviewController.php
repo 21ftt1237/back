056fd43d93bd3ee1787581664567b9c8538748f3
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-11 19:41:17 +0800

    Update ReviewController.php

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -9,33 +9,32 @@ class ReviewController extends Controller
 {
     
  public function store(Request $request)
-{
-    // Validate the request data
-    $request->validate([
-        'review' => 'required|string|max:255',
-        'rating' => 'required|integer',
-    ]);
+    {
+        $user = Auth::user(); // Get the currently logged-in user
 
-    // Get the currently authenticated user
-    $user = auth()->user();
+        // Assuming you have a variable $storeNumber available
+        $storeNumber = $request->input('storeNumber');
 
-    // Find the store by store number
-    $store = Store::where('store_number', $storeNumber)->first();
+        // Fetch store based on the store number
+        $store = Store::where('store_number', $storeNumber)->first();
 
-    // Check if the store exists
-    if (!$store) {
-        return response()->json(['error' => 'Store not found.'], 404);
+        // Check if the store exists
+        if (!$store) {
+            return response()->json(['error' => 'Store not found'], 404);
+        }
+
+        // Create a new review
+        $review = new Review([
+            'user_id' => $user->id,
+            'store_id' => $store->id,
+            'review' => $request->input('review'),
+            'rating' => $request->input('rating'),
+        ]);
+
+        // Save the review
+        $review->save();
+
+        return response()->json(['success' => true]);
     }
-
-    // Insert the review into the database with the authenticated user ID and store ID
-    $review = Review::create([
-        'user_id' => $user->id,
-        'store_id' => $store->id,
-        'review' => $request->input('review'),
-        'rating' => $request->input('rating'),
-    ]);
-
-    return response()->json($review, 201); // Return the inserted review data
-}
     
 }
