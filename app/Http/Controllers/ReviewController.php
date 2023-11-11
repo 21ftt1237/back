542e8a2b9e1c773bfdc0d84604f96e58bfb05afb
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-11 21:57:50 +0800

    Update ReviewController.php

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Review;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
 {
