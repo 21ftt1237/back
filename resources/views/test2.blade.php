b92c93a39841ee9e53bcce5721f6042aac272ec0
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-11 19:17:47 +0800

    Update test2.blade.php

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -1,2 +1,46 @@
-
-<div>haha</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Sender</title>
+</head>
+    <style>
+        .container{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
+<body>
+<div class="container">
+    <form onsubmit="sendEmail(); reset(); return false;">
+        <h3>Test help</h3>
+        <input type="text" id="name" placeholder="Your Name" required>
+        <input type="email" id="email" placeholder="Email id" required>
+        <input type="text" id="phone" placeholder="Phone Number" required>
+        <textarea id="message" rows="4" placeholder="How can we help you?"></textarea>
+        <button type="submit">Send</button>
+    </form>
+    
+</div>
+    <script src=" https://smtpjs.com/v3/smtp.js"></script>
+    <script>
+        function sendEmail(){
+            Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "Bruzonestore@gmail.com",
+            Password : "Bruzone2023",
+            To : 'hafiysyahrulnizam@gmail.com',
+            From : document.getElementById("email").value,
+            Subject : "New Email",
+            Body : "And this is the body"
+        }).then(
+          message => alert(message)
+        );
+        }
+    </script>
+</body>
+</html>
