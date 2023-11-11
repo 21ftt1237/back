ddaa3b6b3bd5282706c4e907f3a76e494e66777d
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-11 18:47:59 +0800

    Update ReviewController.php

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Review;
 
 class ReviewController extends Controller
 {
-    //
+   public function store(Request $request)
+{
+    // Validate the request data
+    $request->validate([
+        'store_id' => 'required|exists:stores,id',
+        'review' => 'required|string|max:255',
+        'rating' => 'required|integer',
+    ]);
+
+    // Get the currently authenticated user
+    $user = auth()->user();
+
+    // Insert the review into the database with the authenticated user ID
+    $review = Review::create([
+        'user_id' => $user->id,
+        'store_id' => $request->input('store_id'),
+        'review' => $request->input('review'),
+        'rating' => $request->input('rating'),
+    ]);
+
+    return response()->json($review, 201); // Return the inserted review data
+}
 }
