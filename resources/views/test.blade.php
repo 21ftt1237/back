3f60c21fafb1de38b3b2af18375ea4bc2b87d218
21ftt1225 <98934997+21ftt1225@users.noreply.github.com>
2023-11-12 15:51:40 +0800

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,40 +1,30 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
-    $to = "Bruzonestore@gmail.com";
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
-    $headers = "From: " . $_POST["email"];
-
-    // Send email
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent successfully!";
-    } else {
-        echo "Error sending email.";
-    }
-}
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
+    <title>Contact</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 <body>
-    <h2>Contact Us</h2>
-    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-        <label for="email">Your Email:</label>
-        <input type="email" name="email" required><br>
-
-        <label for="subject">Subject:</label>
-        <input type="text" name="subject" required><br>
-
-        <label for="message">Message:</label>
-        <textarea name="message" rows="4" required></textarea><br>
-
-        <input type="submit" value="Send Email">
+    <h1>Contact</h1>
+    
+    <form method="post" action="send-email.php">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" required>
+        
+        <label for="email">email</label>
+        <input type="email" name="email" id="email" required>
+        
+        <label for="subject">Subject</label>
+        <input type="text" name="subject" id="subject" required>
+        
+        <label for="message">Message</label>
+        <textarea name="message" id="message" required></textarea>
+        
+        <br>
+        
+        <button>Send</button>
     </form>
+    
 </body>
 </html>
