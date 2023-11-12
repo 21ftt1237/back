aa449a2abf804ab9ec2f1470d7aa641a46681d22
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-12 18:18:46 +0800

    Update netcom.blade.php

--- resources/views/netcom.blade.php
+++ resources/views/netcom.blade.php
@@ -1520,22 +1520,32 @@ function displayReviews(reviews, storeNumber) {
   var reviewContainer = document.getElementById('review-container');
   // Clear existing reviews in the container
   reviewContainer.innerHTML = '';
+
   // Filter reviews for the specified store
-  var filteredReviews = reviews.filter(function(review) {
-    return review.storeNumber === storeNumber;
+  var filteredReviews = reviews.filter(function (review) {
+    return review.store_id === storeNumber; // Assuming store_id is the correct property name
   });
+
   // Loop through each review in the filtered array
   for (var i = 0; i < filteredReviews.length; i++) {
     // Create a new div element for each review
     var newReview = document.createElement('div');
     newReview.className = 'review';
+
     // Create the star rating container for the current review
     var starContainer = ReviewStarContainer(filteredReviews[i].rating);
+
     // Create the content container for the current review
-    var contentContainer = ReviewContentContainer(filteredReviews[i].user.name, filteredReviews[i].created_at, filteredReviews[i].review);
+    var contentContainer = ReviewContentContainer(
+      filteredReviews[i].user.name,
+      filteredReviews[i].created_at,
+      filteredReviews[i].review
+    );
+
     // Append the star rating and content containers to the new review div
     newReview.appendChild(starContainer);
     newReview.appendChild(contentContainer);
+
     // Append the new review div to the review container
     reviewContainer.appendChild(newReview);
   }
@@ -1604,7 +1614,7 @@ window.addEventListener('load', function () {
       }
 
       // Display the fetched reviews
-      displayReviews(data.reviews);
+     displayReviews(reviews.reviews, store_id);
     })
     .catch(error => {
       console.error('Error:', error);
