e97d015f238e9da1be13e25f9a07155b1c2c0a63
21ftt1225 <98934997+21ftt1225@users.noreply.github.com>
2023-11-12 17:09:15 +0800

    Update BruzoneEmail.blade.php

--- resources/views/Help/BruzoneEmail.blade.php
+++ resources/views/Help/BruzoneEmail.blade.php
@@ -440,43 +440,7 @@ header .shopping img{
   }
 
 
-    const nodemailer = require("nodemailer");
 
-// Set up nodemailer transporter
-const transporter = nodemailer.createTransport({
-  service: "gmail", // Use the email service provider you prefer
-  auth: {
-    user: "Bruzonestore@gmail.com", // Your email address
-    pass: "Bruzone2023" // Your email password or an app-specific password
-  }
-});
-
-// Function to send an email
-function sendEmail(toEmail, subject, htmlContent) {
-  // Email options
-  const mailOptions = {
-    from: "Bruzonestore@gmail.com", // Your email address
-    to: toEmail,
-    subject: subject,
-    html: htmlContent
-  };
-
-  // Send email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error sending email:", error);
-    } else {
-      console.log("Email sent:", info.response);
-    }
-  });
-}
-
-// Example usage
-const toEmail = "hafiysyahrulnizam@gmail.com";
-const subject = "Subject of your email";
-const htmlContent = "<p>This is your HTML content</p>";
-
-sendEmail(toEmail, subject, htmlContent);
 
 
 </script>
