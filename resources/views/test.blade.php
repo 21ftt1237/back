f1f6002bcdeaf1fd0322417a50ba1534c7763976
21ftt1225 <98934997+21ftt1225@users.noreply.github.com>
2023-11-12 16:42:41 +0800

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,28 +3,42 @@
 <head>
     <title>Contact</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    
 </head>
 <body>
     <h1>Contact</h1>
     
-    <form method="post" action="send-email.php">
+    <form onsubmit="sendEmail(); reset(); return false;">
         <label for="name">Name</label>
         <input type="text" name="name" id="name" required>
         
         <label for="email">email</label>
         <input type="email" name="email" id="email" required>
         
-        <label for="subject">Subject</label>
-        <input type="text" name="subject" id="subject" required>
+        <label for="phone">phone</label>
+        <input type="text" name="phone" id="phone" required>
         
         <label for="message">Message</label>
         <textarea name="message" id="message" required></textarea>
         
         <br>
         
-        <button>Send</button>
+        <button type="submit">Send</button>
     </form>
-    
+    <script src="https://smtpjs.com/v3/smtp.js">
+    function sendEmail(){
+        Email.send({
+            Host : "smtp.gmail.com",
+            Username : "bruzonedept@gmail.com",
+            Password : "klfc slad fbvp mlht",
+            To : 'bruzonedept@gmail.com',
+            From : document.getElementById("email").value,
+            Subject : "hi qebs",
+            Body : "And this is the body"
+        }).then(
+      message => alert(message)
+    );
+    }
+</script>
 </body>
 </html>
