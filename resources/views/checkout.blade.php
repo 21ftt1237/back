fc584a954dd838806d7477ec8ca535a04f1bbab8
21ftt1236 <98957127+21ftt1236@users.noreply.github.com>
2023-11-13 12:07:12 +0800

    Update checkout.blade.php

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1004,25 +1004,25 @@ if (couponPointsGained !== null && couponPointsGained !== undefined) {
   }
 
 
-  function updateTotalPriceAndDeliveryFee() {
-  
-  
-const totalAmount = parseFloat(localStorage.getItem('totalPrice')) 
-   
-  const deliveryFee = parseFloat(localStorage.getItem('delivery')) || 0;
+const deliveryFee = parseFloat(localStorage.getItem('delivery')) || 0;
+const storeName = localStorage.getItem('storename');
 
-      const testPay = totalAmount + deliveryFee;
- 
-  const finalPay = totalAmount + deliveryFee - {{ auth()->user()->redeem_coupon }};
+const feeDiv = document.getElementById('fee');
 
-  
-  document.getElementById('pay').innerHTML = `<h4>Final Total: BND ${finalPay.toFixed(2)}`;
-  
+if (deliveryFee) {
+  feeDiv.innerHTML = <h4>Delivery Fee: BND ${deliveryFee}</h4>;
+} else {
+  feeDiv.innerHTML = "<h4>Delivery Fee not found</h4> }
 
-  
+function updateTotalPriceAndDeliveryFee() {
+  const totalAmount = parseFloat(localStorage.getItem('totalPrice')) || 0;
+  const couponDiscount = parseFloat({{ auth()->user()->redeem_coupon }}) || 0;
+
+  const finalPay = totalAmount + deliveryFee - couponDiscount;
+
+  document.getElementById('pay').innerHTML = <h4>Final Total: BND ${finalPay.toFixed(2)};
   localStorage.setItem('finalPay', finalPay.toFixed(2));
 }
-
 
 updateTotalPriceAndDeliveryFee();
 
@@ -1072,32 +1072,6 @@ updateTotalPriceAndDeliveryFee();
 
         
         map.setView([lat, lon], 13);
-
-// Replace the following line with the actual number of stores you have
-const numberOfStores = 6;
-
-// Function to calculate the total delivery fee
-function calculateTotalDeliveryFee() {
-  let totalDeliveryFee = 0;
-
-  // Loop through each store
-  for (let i = 1; i <= numberOfStores; i++) {
-    // Replace 'deliveryFeeStoreX' with the actual delivery fee variable for each store
-    const deliveryFeeStoreX = parseFloat(localStorage.getItem(`deliveryStore${i}`)) || 0;
-
-    // Add the delivery fee of the current store to the total
-    totalDeliveryFee += deliveryFeeStoreX;
-  }
-
-  return totalDeliveryFee;
-}
-
-// Example usage
-const totalDeliveryFee = calculateTotalDeliveryFee();
-console.log(`Total Delivery Fee for ${numberOfStores} stores: BND ${totalDeliveryFee.toFixed(2)}`);
-
-
-
 
      
     </script>
