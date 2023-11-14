af2886b0d83826d8d94f2c36c20448b9e575ec00
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-14 14:08:23 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\WishlistController;
