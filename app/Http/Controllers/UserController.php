1451e7da4d3cbcebf8d1daec617a1cc1a09a1a32
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-15 15:38:08 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,20 +1,66 @@
 <?php
-use App\Models\User;
-
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-     public function getUsers()
+    public function index()
     {
         // Fetch data from the 'users' table
-        $users = User::select('id', 'name', 'email', 'password', 'role_id')->get();
+        $users = User::select('id', 'name', 'email', 'role_id')->get();
 
         // Convert data to JSON for JavaScript
         return response()->json($users);
     }
-}
+
+    public function show($id)
+    {
+        // Fetch a specific user
+        $user = User::findOrFail($id);
+
+
+        return response()->json($user);
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Validate the request
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique('users')->ignore($id),
+            ],
+            'role_id' => 'required|integer',
+        ]);
+
+        // Fetch the user
+        $user = User::findOrFail($id);
+
+
+        // Update user details
+        $user->update($request->all());
+
+        return response()->json($user);
+    }
+
+    public function destroy($id)
+    {
+        // Fetch the user
+        $user = User::findOrFail($id);
+
+    
+        // Delete the user
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully']);
+    }
 }
