928fcf87d88a9f1eb0e5b0bd274b18e249b6e5f7
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-15 21:24:57 +0800

    Update VerificationController.php

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -39,4 +39,13 @@ class VerificationController extends Controller
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
+
+    protected function verified(Request $request, $user)
+    {
+        if ($user->isAdmin()) {
+            return redirect()->route('dashboard.admin');
+        }
+
+        return redirect()->route('dashboard.user');
+    }
 }
