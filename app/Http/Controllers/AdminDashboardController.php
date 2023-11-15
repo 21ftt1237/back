e64613b9a8b4888e9fb7d633ac2d8938a14094bd
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-15 15:56:28 +0800

    Update AdminDashboardController.php

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User; // Assuming your user model is in the App\Models namespace
 
 class AdminDashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::all(); 
+
+        return view('Dashboard-adm', ['users' => $users]);
+    }
 }
+
