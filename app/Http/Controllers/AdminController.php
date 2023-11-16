147b989a50bc9c6da345d71793311b57f57db1cb
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-17 05:16:28 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,12 +14,32 @@ class AdminController extends Controller
         $admins = Admin::with('role_id')->get();
 
       
-        return view('admin.adminDashboard', compact('admins'));
+        return view('Dashboard-adm', compact('admins'));
     }
 
     public function ChangeStatuss (Request $request){
 
 
 }
+
+    public function store(Request $request)
+    {
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:admins',
+            'password' => 'required|string|min:8',
+        ]);
+
+        // Create a new admin
+        $admin = new Admin;
+        $admin->name = $validatedData['name'];
+        $admin->email = $validatedData['email'];
+        $admin->password = bcrypt($validatedData['password']);
+        $admin->save();
+
+        // Redirect back or to a success page
+        return redirect()->back()->with('success', 'Admin added successfully');
+    }
 
 }
