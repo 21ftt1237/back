851ce452c98a4741cbf1874b69f7d2365d6ee492
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-17 05:08:00 +0800

    Update Dashboard-adm.blade.php

--- resources/views/Dashboard-adm.blade.php
+++ resources/views/Dashboard-adm.blade.php
@@ -11,8 +11,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="./ecommerce.css">
   <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     
 
   <!-- Other meta tags and styles -->
