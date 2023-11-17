45042911d684c1f6e77ab6e1adc7a0d321c09500
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-18 03:44:45 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -31,5 +31,42 @@ class AdminController extends Controller
 
         return redirect()->route('dashboard.admin')->with('success', 'Admin added successfully');
     }
+
+
+    public function handleForm(Request $request, $action)
+{
+    
+    if ($action == 'store') {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $defaultRoleId = 1; 
+        $admin = User::create($validatedData + ['role_id' => $defaultRoleId]);
+
+        return redirect()->route('dashboard.admin')->with('success', 'Admin added successfully');
+    
+    } elseif ($action == 'delete') {
+       $request->validate([
+            'userId' => 'required|exists:users,id',
+        ]);
+
+        $userId = $request->input('userId');
+        $user = User::find($userId);
+
+        if ($user) {
+            $user->delete();
+            return redirect()->back()->with('success', 'User deleted successfully');
+        }
+
+        return redirect()->back()->with('error', 'User not found');
+    }
+    
+
+    // Redirect back to the Dashboard-adm view or wherever is appropriate
+    return redirect()->route('dashboard.admin')->with('success', 'Form submitted successfully');
+}
 }
 
