5db565cd71e3716ca182f45e639ad81f52490708
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-17 23:08:46 +0800

    Update Guardian.blade.php

--- resources/views/Guardian.blade.php
+++ resources/views/Guardian.blade.php
@@ -1308,11 +1308,11 @@ display: block; /* Change display to block to stack elements vertically */
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        
+         window.location.reload();
     })
     .catch(error => {
         console.error('Error:', error);
-        
+         window.location.reload();
     });
     
 });
