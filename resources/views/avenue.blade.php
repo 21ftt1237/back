6084950b507ae9f9b8cad91baa39f6ca75bf9bef
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-17 23:09:36 +0800

    Update avenue.blade.php

--- resources/views/avenue.blade.php
+++ resources/views/avenue.blade.php
@@ -1266,11 +1266,11 @@ height: 1600px
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        // Handle the response as needed (e.g., display a success message)
+        window.location.reload();
     })
     .catch(error => {
         console.error('Error:', error);
-        // Handle errors if any
+        window.location.reload();
     });
     // Additional logic can be added here if needed
 });
@@ -1281,7 +1281,7 @@ window.addEventListener('load', function () {
     .then(response => response.json())
     .then(data => {
       console.log(data);
-
+       
       if (data.debug) {
         console.log(data.debug);
       }
