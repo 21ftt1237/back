7a2743ea2ea88f381a6834ddeb1d6da0eaec8f3a
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-18 22:31:33 +0800

    Create order-status-updated.blade.php

---
+++ resources/views/emails/order-status-updated.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        /* Add your email styles here */
+        body {
+            font-family: 'Helvetica Neue', 'Helvetica', sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 0;
+            color: #333;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+        }
+
+        .header {
+            background-color: #f4f4f4;
+            padding: 20px;
+            text-align: center;
+        }
+
+        .content {
+            padding: 20px;
+        }
+
+        .footer {
+            background-color: #f4f4f4;
+            padding: 10px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h2>Order Status Updated</h2>
+        </div>
+
+        <div class="content">
+            <p>Your order status has been updated. Thank you for choosing our services.</p>
+        </div>
+
+        <div class="footer">
+            <p>If you have any questions, please contact our support team.</p>
+        </div>
+    </div>
+</body>
+</html>
