9eec0b961b80a03b74220f98144883f542518547
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-19 21:28:24 +0800

    Update OwnerController.php

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Repository\IOwnerRepository;
 use Illuminate\Http\Request;
 
 class OwnerController extends Controller
 {
-    //
+
+    public $owner;
+
+    public function __construct(IOwnerRepository $owner)
+    {
+        $this->owner = $owner;
+    }
+
+
+    public function ownerShowAllProduct() {
+        $products =  $this->owner->adminShowAllProduct();
+        return view('owner.netcom')->with('products', $products);
+    }
+
+
+    public function ownerDeleteProduct($id) {
+        $this->owner->ownerDeleteProduct($id);
+        return redirect('/owner/products');
+    }
+
 }
