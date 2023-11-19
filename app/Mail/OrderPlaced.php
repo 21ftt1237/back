7429ca0f396dd2c444d49f3f224f2e4e86bac249
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-19 19:18:26 +0800

    Update OrderPlaced.php

--- app/Mail/OrderPlaced.php
+++ app/Mail/OrderPlaced.php
@@ -5,13 +5,13 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class OrderPlaced extends Mailable
 {
     use Queueable, SerializesModels;
+
+    public $orderDetails;
 
     /**
      * Create a new message instance.
@@ -20,31 +20,13 @@ class OrderPlaced extends Mailable
     {
         $this->orderDetails = $orderDetails;
     }
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Order Placed',
-        );
-    }
 
     /**
-     * Get the message content definition.
+     * Build the message.
      */
     public function build()
     {
-        return $this->view('emails.order-placed');
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->view('emails.order-placed')
+            ->subject('Order Placed');
     }
 }
