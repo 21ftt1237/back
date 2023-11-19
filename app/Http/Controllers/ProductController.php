2311f25b0263daadb5f4401685febf406707ca9f
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-19 23:37:02 +0800

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -50,7 +50,7 @@ class ProductController extends Controller
     }
 
     
-    public function index1($storeId)
+    public function index1($viewName)
     {
         $store = DB::table('stores')->where('id', $storeId)->first();
         
