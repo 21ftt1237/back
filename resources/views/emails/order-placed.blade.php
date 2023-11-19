0bb026ea402b4964e91c15e919642dc922b6a330
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-19 22:37:39 +0800

    Update order-placed.blade.php

--- resources/views/emails/order-placed.blade.php
+++ resources/views/emails/order-placed.blade.php
@@ -4,35 +4,13 @@
     <title>Checkout</title>
 </head>
 <body>
-
-    <!-- Your existing code -->
-
-    @if(isset($cart) && $cart->isNotEmpty()) <!-- Check if $cart is set and not empty -->
-        <table>
-            <thead>
-                <tr>
-                    <th>Product</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Total Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($cart as $cartItem)
-                    <tr>
-                        <td>{{ $cartItem['product_name'] }}</td>
-                        <td>${{ $cartItem['price'] }}</td>
-                        <td>{{ $cartItem['quantity'] }}</td>
-                        <td>${{ $cartItem['total_price'] }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @else
-        <p>No items in the cart.</p>
-    @endif
-
-    <!-- Your existing code -->
+    @foreach ($orderDetails as $order)
+    <p>Product: {{ $order['product_name'] }}</p>
+    <p>Price: ${{ $order['price'] }}</p>
+    <p>Quantity: {{ $order['quantity'] }}</p>
+    <p>Total Price: ${{ $order['total_price'] }}</p>
+    <hr>
+@endforeach
 
 </body>
 </html>
