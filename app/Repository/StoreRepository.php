06fc9c46fa1e87578c8010be1e377d66b64405a5
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-20 10:03:05 +0800

    Update StoreRepository.php

--- app/Repository/StoreRepository.php
+++ app/Repository/StoreRepository.php
@@ -3,9 +3,10 @@ namespace App\Repository;
 
 use Illuminate\Support\Facades\DB;
 
-class StoreRepository
+class StoreRepository implements IStoreRepository
 {
     public function getSingleStoreByName($storeName)
     {
         return DB::table('stores')->where('name', $storeName)->first();
     }
+}
