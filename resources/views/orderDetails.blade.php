89d04d09b455e2f87d2b8e82914e56b64b1f54e6
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-20 15:14:08 +0800

    Update orderDetails.blade.php

--- resources/views/orderDetails.blade.php
+++ resources/views/orderDetails.blade.php
@@ -1,44 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order Details</title>
-    <!-- Add your CSS links or styles here -->
-</head>
-<body>
-   <div class="container">
+<div class="container">
         <h1>All Orders Details</h1>
 
-        @if($orderDetails->count() > 0)
-            @forelse($orderDetails as $order)
-                <div class="order-details">
-                    <p>Order ID: {{ $order->id }}</p>
-                    <p>Order Date: {{ $order->created_at }}</p>
-                    <!-- Add more details as needed -->
+        @forelse($orderDetails as $order)
+            <div class="order-details">
+                <p>Order ID: {{ $order->id }}</p>
+                <p>Order Date: {{ $order->created_at }}</p>
+                <!-- Add more details as needed -->
 
-                    <!-- Fetch product details related to the order -->
-                    @php
-                        $productDetails = App\Models\Product::where('id', $order->product_id)->first();
-                    @endphp
+                <!-- Fetch product details related to the order -->
+                @php
+                    $productDetails = App\Models\Product::find($order->product_id);
+                @endphp
 
-                    <!-- Display product details -->
-                    @if($productDetails)
-                        <p>Product Name: {{ $productDetails->name }}</p>
-                        <p>Product Price: {{ $productDetails->price }}</p>
-                        <img src="{{ $productDetails->image }}" alt="Product Image">
-                        <!-- Add more fields based on your actual product schema -->
-                    @else
-                        <p>Product not found.</p>
-                    @endif
-                </div>
-                <hr>
-            @empty
-                <p>No orders found.</p>
-            @endforelse
-        @else
+                <!-- Display product details -->
+                @if($productDetails)
+                    <p>Product Name: {{ $productDetails->name }}</p>
+                    <p>Product Price: {{ $productDetails->price }}</p>
+                    <img src="{{ $productDetails->image }}" alt="Product Image">
+                    <!-- Add more fields based on your actual product schema -->
+                @else
+                    <p>Product not found.</p>
+                @endif
+            </div>
+            <hr>
+        @empty
             <p>No orders found.</p>
-        @endif
+        @endforelse
     </div>
-</body>
-</html>
