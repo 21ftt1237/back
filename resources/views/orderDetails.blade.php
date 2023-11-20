a893d2a56e87e751d636df2f23c036defd1dd01b
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-20 15:17:00 +0800

    Update orderDetails.blade.php

--- resources/views/orderDetails.blade.php
+++ resources/views/orderDetails.blade.php
@@ -1,22 +1,22 @@
 <div class="container">
-        <h1>Order Details</h1>
+    <h1>Order Details</h1>
 
-        @if($orderDetails)
-            <p>Order ID: {{ $orderDetails->id }}</p>
-            <p>Order Date: {{ $orderDetails->created_at }}</p>
+    @forelse($orderDetails as $order)
+        <div class="order-details">
+            <p>Order ID: {{ $order->id }}</p>
+            <p>Order Date: {{ $order->created_at }}</p>
             <!-- Add more details as needed -->
 
-            <!-- Display product details -->
-            @if($productDetails)
-                <p>Product Name: {{ $productDetails->name }}</p>
-                <p>Product Price: {{ $productDetails->price }}</p>
-                <img src="{{ $productDetails->image }}" alt="Product Image">
+            <!-- Fetch and display product details for each order -->
+            @foreach($order->products as $product)
+                <p>Product Name: {{ $product->name }}</p>
+                <p>Product Price: {{ $product->price }}</p>
+                <img src="{{ $product->image }}" alt="Product Image">
                 <!-- Add more fields based on your actual product schema -->
-            @else
-                <p>Product not found.</p>
-            @endif
-
-        @else
-            <p>Order not found.</p>
-        @endif
-    </div>
+            @endforeach
+        </div>
+        <hr>
+    @empty
+        <p>No orders found.</p>
+    @endforelse
+</div>
