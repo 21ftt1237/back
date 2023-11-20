5314980b4b2ae671a029a318b72073df07bbddf0
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-20 13:23:49 +0800

    Update order-placed.blade.php

--- resources/views/emails/order-placed.blade.php
+++ resources/views/emails/order-placed.blade.php
@@ -3,36 +3,83 @@
 
 <head>
     <title>Order Placed</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        p {
+            color: #555;
+            margin: 10px 0;
+        }
+
+        .order-details {
+            border: 1px solid #ddd;
+            padding: 10px;
+            margin: 10px 0;
+            background-color: #fff;
+        }
+
+        .order-details p {
+            margin: 5px 0;
+        }
+
+        .thank-you {
+            background-color: #4CAF50;
+            color: #fff;
+            padding: 10px;
+            text-align: center;
+            margin: 20px 0;
+        }
+
+        .best-regards {
+            margin-top: 20px;
+            text-align: center;
+            color: #777;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Order Placed Successfully</h1>
+    <div class="order-details">
+        <h1>Order Placed Successfully</h1>
 
-    <p>Thank you for your order!</p>
+        <p>Thank you for your order!</p>
 
-    <p>User Email: {{ $userEmail }}</p>
+        <p>User Email: {{ $userEmail }}</p>
 
-    @foreach ($orderDetails as $createdAt => $orders)
-        @foreach ($orders as $order)
-            {{-- Log order information for debugging --}}
-            @php
-                Log::info('Order Details: ' . json_encode($order));
-            @endphp
+        @foreach ($orderDetails as $createdAt => $orders)
+            @foreach ($orders as $order)
+                {{-- Log order information for debugging --}}
+                @php
+                    Log::info('Order Details: ' . json_encode($order));
+                @endphp
 
-            {{-- Check if the expected keys exist --}}
-            @if (isset($order['product_name']) && isset($order['product_price']))
-                <p>Product: {{ $order['product_name'] }}</p>
-                <p>Price: ${{ $order['product_price'] }}</p>
-                <p>Quantity: {{ $order['quantity'] }}</p>
-                <p>Total Price: ${{ $order['Total_price'] }}</p>
-                <!-- Add other fields as needed -->
-            @endif
+                @if ($order['product']) {{-- Check if product is not null --}}
+                    <div class="product-details">
+                        <p>Product: {{ $order['product']['name'] }}</p>
+                        <p>Price: ${{ $order['product']['price'] }}</p>
+                        <p>Quantity: {{ $order['quantity'] }}</p>
+                        <p>Total Price: ${{ $order['Total_price'] }}</p>
+                        <!-- Add other fields as needed -->
+                    </div>
+                @endif
+            @endforeach
         @endforeach
-    @endforeach
 
-    <p>Thank you for choosing our services.</p>
+        <div class="thank-you">
+            <p>Thank you for choosing our services.</p>
+        </div>
 
-    <p>Best regards,<br> Your Company Name</p>
+        <p class="best-regards">Best regards,<br> Bruzone</p>
+    </div>
 </body>
 
 </html>
