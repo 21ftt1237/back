ca9c22d5c8973732c9e6dcedab94a1c2d2d26a4a
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-20 13:59:11 +0800

    Update order-status-updated.blade.php

--- resources/views/emails/order-status-updated.blade.php
+++ resources/views/emails/order-status-updated.blade.php
@@ -70,7 +70,7 @@
        </div>      
 
         <div class="section1">
-            <p>Your order with ID <strong>{{ $orderId }}</strong> status has been updated. Thank you for choosing our services.</p>
+            <p>Your order with ID <strong>{{ $orderId }}</strong> status has been updated to <strong>{{ $newStatus }}</strong> . Thank you for choosing our services.</p>
         </div>
 
         <div class="footer">
