3d180b09aa72d3f6bed310bcb0db6680112cee31
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-22 13:59:06 +0800

    Update IComnetRepository.php

--- app/Repository/IComnetRepository.php
+++ app/Repository/IComnetRepository.php
@@ -1,1 +1,23 @@
 
+<?php
+namespace App\Repository;
+
+interface IProductRepository {
+
+    public function getAllProducts();
+
+    public function getSingleProduct($id);
+
+    public function createProduct(array $data);
+
+    public function editProduct($id);
+
+    public function updateProduct($id, array $data);
+
+
+}
+
+
+
+
+?>
