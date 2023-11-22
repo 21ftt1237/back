f6d543f08219c04bb0c87bf7771494b283813288
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-22 14:26:42 +0800

    Update IComnetRepository.php

--- app/Repository/IComnetRepository.php
+++ app/Repository/IComnetRepository.php
@@ -3,15 +3,9 @@ namespace App\Repository;
 
 interface IComnetRepository {
 
-    public function getAllProducts();
+    public function comnetShowAllProduct();
 
-    public function getSingleProduct($id);
-
-    public function createProduct(array $data);
-
-    public function editProduct($id);
-
-    public function updateProduct($id, array $data);
+    public function comnetDeleteProduct($id);
 
 
 }
