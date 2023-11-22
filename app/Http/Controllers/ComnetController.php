796312646ab42c6db524ea4c228c2ed481c4b0ce
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-22 13:57:38 +0800

    Update ComnetController.php

--- app/Http/Controllers/ComnetController.php
+++ app/Http/Controllers/ComnetController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Repository\IAdminRepository;
+use App\Repository\IComnetRepository;
 use Illuminate\Http\Request;
 
 class ComnetController extends Controller
