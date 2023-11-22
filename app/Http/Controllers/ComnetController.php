4e6856e41abc89b4604349dfeca9e67667dec7e1
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-22 13:56:46 +0800

    Update ComnetController.php

--- app/Http/Controllers/ComnetController.php
+++ app/Http/Controllers/ComnetController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Repository\IAdminRepository;
 use Illuminate\Http\Request;
 
 class ComnetController extends Controller
 {
-    //
+
+    public $comnet;
+
+    public function __construct(IComnetRepository $comnet)
+    {
+        $this->comnet = $comnet;
+    }
+
+
+    public function comnetShowAllProduct() {
+        $products =  $this->comnet->comnetShowAllProduct();
+        return view('comnet.index')->with('products', $products);
+    }
+
+
+    public function comnetDeleteProduct($id) {
+        $this->comnet->comnetDeleteProduct($id);
+        return redirect('/comnet/products');
+    }
+
 }
