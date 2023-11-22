647f6b05ee4a7f397e38a2b58b6377e03ff244bc
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-22 14:06:30 +0800

    Update ComnetRepository.php

--- app/Repository/ComnetRepository.php
+++ app/Repository/ComnetRepository.php
@@ -1,1 +1,55 @@
 
+<?php
+
+namespace App\Repository;
+
+use App\Models\Product;
+
+class ProductRepository implements IProductRepository {
+
+    public function getAllProducts()
+    {
+       return Product::all();
+    }
+
+    public function getSingleProduct($id)
+    {
+
+        return  Product::find($id);
+
+    }
+
+    public function createProduct(array $data)
+    {
+
+        $product = new Product();
+        $product->picture = $data['image_link'];
+        $product->title = $data['name'];
+        $product->price = $data['price'];
+        $product->description = $data['description'];
+
+
+        $product->save();
+
+    }
+
+    public function editProduct($id)
+    {
+        return Product::find($id);
+    }
+
+    public function updateProduct($id, array $data)
+    {
+       Product::find($id)->update([
+        $product->picture = $data['image_link'];
+        $product->title = $data['name'];
+        $product->price = $data['price'];
+        $product->description = $data['description'];
+        ]);
+    }
+
+}
+
+
+
+?>
