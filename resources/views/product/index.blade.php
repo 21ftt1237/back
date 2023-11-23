37a19bd9afbbb51c77b2c3cc82f13a5af5520d23
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-23 12:28:43 +0800

    Update index.blade.php

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -18,7 +18,7 @@
                   <h5 class="card-title">Price: ${{ $product->price }}</h5>
                   <hr>
                   <p class="card-text">{{ $product->description}} </p>
-
+                  <a href="{{ route('product.show', $product->id) }}" class="btn btn-primary">View Detail</a> -->
                 </div>
               </div>
             @endforeach
