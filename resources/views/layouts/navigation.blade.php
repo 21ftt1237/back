67e5354822fc45ac1d034a080a60ba85a35b8bdd
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-11-23 12:02:21 +0800

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,7 +20,7 @@
                 <!-- Navigation Links for Admin Users -->
 @if (Auth::user() && Auth::user()->isAdmin()) <!-- Assuming isAdmin() is a method to check admin role -->
     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-        <x-nav-link :href="route('admin.adminDashboard')" :active="request()->routeIs('admin.dashboard')">
+        <x-nav-link :href="route('admin.adminDashboard')" :active="request()->routeIs('admin.adminDashboard')">
             {{ __('Admin Dashboard') }}
         </x-nav-link>
     </div>
