495dc2b21e0a05563d53c4b31dc22db17d955460
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-27 19:01:31 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,7 @@ Auth::routes(['verify' => true, 'name' => 'auth.', 'password.reset' => 'password
 //Route::get('/order-list/{orderStatus}', [OrderController::class, 'showOrderList']);
 
 
-Route::get('/products', [ComnetController::class, 'index'])->name('comnet.comnet');
+Route::get('/products', [ComnetController::class, 'indexComnet'])->name('comnet.comnet');
 Route::get('/products/create', [ComnetController::class, 'create'])->name('comnet.create');
 Route::post('/products/store', [ComnetController::class, 'store'])->name('comnet.store');
 
