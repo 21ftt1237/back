b71eda62bf2566bace56b514db0fc61df65fdd70
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-30 13:19:08 +0800

    Update ComnetController.php

--- app/Http/Controllers/ComnetController.php
+++ app/Http/Controllers/ComnetController.php
@@ -69,8 +69,8 @@ class ComnetController extends Controller
 
         if ($image = $request->file('image_link')) {
         $name = time() . '.' . $image->getClientOriginalName();
-        $path = $image->storeAs('public/image', $name, 'public');
-        $data['image_link'] =  $name;
+        $path = $image->storeAs('image', $name, 'public');
+        $data['image_link'] =  '/image/' . $name;
 
     }
         $this->comnet->comnetCreateProduct($data);
