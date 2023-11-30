00544bea08d62580770329461e8831512775cd04
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-12-01 00:05:05 +0800

    Create ISimanjaRepository.php

---
+++ app/Repository/ISimanjaRepository.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Repository;
+
+interface IComnetRepository {
+
+    public function comnetShowAllProduct();
+
+    public function indexComnet();
+
+    public function comnetDeleteProduct($id);
+
+    public function getNewlyAddedProducts();
+
+
+}
+
+
+
+
+?>
