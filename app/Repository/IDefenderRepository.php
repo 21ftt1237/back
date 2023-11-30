39ee655ac16af9a0e28d055dde22074aa6dbc212
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-12-01 01:14:18 +0800

    Update IDefenderRepository.php

--- app/Repository/IDefenderRepository.php
+++ app/Repository/IDefenderRepository.php
@@ -5,8 +5,6 @@ interface IDefenderRepository {
 
     public function defenderShowAllProduct();
 
-    public function indexComnet();
-
     public function defenderDeleteProduct($id);
 
     public function getNewlyAddedProducts();
