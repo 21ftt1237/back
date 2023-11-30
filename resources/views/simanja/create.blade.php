6434edfb2e43a23a0218c567f965b8ef68ff8667
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-11-30 23:48:41 +0800

    Update create.blade.php

--- resources/views/simanja/create.blade.php
+++ resources/views/simanja/create.blade.php
@@ -10,7 +10,7 @@
     <hr>
 
 
-    <form action="{{ route('comnet.store') }}" enctype="multipart/form-data" method="POST">
+    <form action="{{ route('simanja.store') }}" enctype="multipart/form-data" method="POST">
         @csrf
 
                 <div class="mb-3">
