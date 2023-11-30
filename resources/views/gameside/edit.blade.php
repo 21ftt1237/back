666ce0f1c3a35cbd6d6967f1c60bd5ef97cc3c30
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-12-01 05:18:39 +0800

    Update edit.blade.php

--- resources/views/gameside/edit.blade.php
+++ resources/views/gameside/edit.blade.php
@@ -4,33 +4,33 @@
 
 <div class="container mt-5 mb-5">
 
-    <h2>Create Product</h2>
+    <h2>Update Product</h2>
     <hr>
 
-    <form action="{{ route('gameside.store') }}" enctype="multipart/form-data" method="POST">
+    <form action="{{ route('gameside.update', $product->id) }}" enctype="multipart/form-data" method="POST">
         @csrf
 
         <div class="mb-3">
-            <label for="image_link" class="form-label">Choose Picture</label>
-            <input class="form-control" type="file" name="image_link" id="image_link">
+            <label for="picture" class="form-label">Choose Picture</label>
+            <input class="form-control" type="file" name="picture" id="picture">
           </div>
 
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" name="title" id="title" placeholder="Enter title">
+            <input type="text" class="form-control" name="title" id="title" value="{{ $product->name }}" placeholder="Enter title">
           </div>
 
           <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="text" class="form-control" name="price" id="price" placeholder="Enter price">
+            <input type="text" class="form-control" name="price" id="price" value="{{ $product->price }}" placeholder="Enter price">
           </div>
 
           <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <textarea class="form-control" name="description" id="description" placeholder="Enter Description"></textarea>
+            <textarea class="form-control" name="description" id="description" placeholder="Enter Description">{{ $product->description }}</textarea>
           </div>
 
-          <button type="submit" class="btn btn-primary">Create Product</button>
+          <button type="submit" class="btn btn-primary">Update Product</button>
 
     </form>
 
