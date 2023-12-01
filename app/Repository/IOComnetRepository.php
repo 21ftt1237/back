b08ec9317210b8951a683073ffa95c27c1337458
21ftt1248 <99055542+21ftt1248@users.noreply.github.com>
2023-12-01 09:10:49 +0800

    Update IOComnetRepository.php

--- app/Repository/IOComnetRepository.php
+++ app/Repository/IOComnetRepository.php
@@ -8,6 +8,13 @@ interface IOComnetRepository {
 
     public function ocomnetDeleteProduct($id);
 
+    
+    public function createProduct(array $data);
+
+     public function editProduct($id);
+
+     public function updateProduct($id, array $data);
+
 }
 
 
